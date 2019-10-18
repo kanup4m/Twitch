dbf61bd3baed7d0227059d004e2ebb8f48548fcc
kanup4m <kanupam500@gmail.com>
2019-10-18 21:04:10 +0530

    Updates

---
+++ precache-manifest.9c4955fad8886ff30ad15059a02c8846.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9828452af0443810cc3c00de2687c58",
+    "url": "/Twitch/index.html"
+  },
+  {
+    "revision": "c70881a0a0c99364c539",
+    "url": "/Twitch/static/css/2.4b451d55.chunk.css"
+  },
+  {
+    "revision": "c9882100e3e52b29c51b",
+    "url": "/Twitch/static/css/main.660f2a49.chunk.css"
+  },
+  {
+    "revision": "c70881a0a0c99364c539",
+    "url": "/Twitch/static/js/2.cf9ccb96.chunk.js"
+  },
+  {
+    "revision": "c9882100e3e52b29c51b",
+    "url": "/Twitch/static/js/main.2f783347.chunk.js"
+  },
+  {
+    "revision": "66d741f15667ee842f40",
+    "url": "/Twitch/static/js/runtime-main.b02f4e76.js"
+  }
+]);
